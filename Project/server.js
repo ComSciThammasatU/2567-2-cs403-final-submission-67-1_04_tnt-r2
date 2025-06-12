09552441fcab67ea242da21f7869b7fc5fc4f7d2
KunchayaNillaphand <kunchaya.nil@dome.tu.ac.th>
2025-06-13 02:56:15 +0700

    Update server.js

--- Project/server.js
+++ Project/server.js
@@ -40,7 +40,7 @@
   const pool = mysql.createPool({
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "Missmukuro021",
+    password: process.env.DB_PASSWORD || " ", //your MySQL password
     database: process.env.DB_NAME || "Imedtag",
     waitForConnections: true,
     connectionLimit: 10,
